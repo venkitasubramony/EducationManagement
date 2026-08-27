4f5ffa2bbfdde7a09494b5ec7fd0920cbb31fdcd
venkitasubramony <venkitasubramony@yahoo.com>
2026-08-27 23:15:18 +0530

    reload fix

--- backend/server.js
+++ backend/server.js
@@ -34,9 +34,7 @@ app.use('/api/dashboard', dashboardRoutes)
 app.use('/api/home', homeRoutes)
 app.use('/api/admin', adminRoutes)
 
-// app.get("/", (req, res) => {
-//   res.send("Education Management API is running");
-// });
+
 
 app.get("/api/students", async (req, res) => {
 
